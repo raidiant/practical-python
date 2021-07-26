b69ac3f95aea0ebdf694191fc7b3c1984483b40e
Ben Wiersma <b.j.j.wiersma@gmail.com>
2021-07-26 13:57:12 +0200

    Finish pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+
+    total = 0
+    for line in f:
+        name, shares, price = line.split(',')
+        tot_share_cost = int(shares.strip()) * float(price.strip())
+        print(name, 'total cost', tot_share_cost)
+        total = total + tot_share_cost
+
+print('Total cost', total)
+
