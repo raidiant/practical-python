c410bf092070c50247a89c8eb0b99d06583e6e04
Ben Wiersma <b.j.j.wiersma@gmail.com>
2021-07-26 12:19:45 +0200

    Finish 1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+ratio = 3/5
+bounces = 1
+
+while height > 1:
+    height = height * ratio
+    print(bounces, round(height, 4))
+    bounces = bounces + 1
